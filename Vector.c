217a2afe43f2e0a14599edf63710cceb2b98f8ea
Wu Yanqiao <136869354+Wuyanqiao@users.noreply.github.com>
2024-11-14 20:38:57 +0800

    Add files via upload

--- Vector.c
+++ Vector.c
@@ -7,14 +7,13 @@ void binaryToHex(char *binary[], int n) {
     for (int i = 0; i < n; i++) {
         int value = 0;
         
-        // 转换每个 8 位二进制数为十进制整数
         for (int j = 0; j < 8; j++) {
             if (binary[i][j] == '1') {
-                value |= (1 << (7 - j)); // 从高位到低位设置
+                value |= (1 << (7 - j)); 
             }
         }
 
-        // 输出十六进制数
+        
         printf("Binary: %s -> Hex: %#X\n", binary[i], value);
     }
 }
--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main() {
     printf("Enter the number of binary control codes: ");
     scanf("%d", &n);
 
-    // 动态分配内存存储二进制控制码
+    
     char **binary = (char **)malloc(n * sizeof(char *));
     for (int i = 0; i < n; i++) {
         binary[i] = (char *)malloc(9 * sizeof(char)); // 8位 + 结束符
@@ -16,10 +16,9 @@ int main() {
         scanf("%s", binary[i]);
     }
 
-    // 调用转换函数
     binaryToHex(binary, n);
 
-    // 释放内存
+    
     for (int i = 0; i < n; i++) {
         free(binary[i]);
     }
