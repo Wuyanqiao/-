bc31105718e98ed41a0c532a625402f6791dbaf3
Wu Yanqiao <136869354+Wuyanqiao@users.noreply.github.com>
2024-11-20 21:45:14 +0800

    Update and rename Array.h to HW3_Array.h

--- Array.h
+++
@@ -1,7 +0,0 @@
-//#pragma once
-#ifndef _ARRAY_H_
-#define _ARRAY_H_
-double** arradd(double** arr1, double** arr2, int n, int q, int p);
-double** arrmul(double** arr1, double** arr2, int n, int q);
-double**arrtransp(double** a, int n, int m);
-#endif
